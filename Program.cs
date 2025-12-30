ccb741bd66a6ef6a02f649fb14f2f0285ba9f0fd
mrange <marten_range@hotmail.com>
2025-12-30 22:06:45 +0100

    Landscape

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.Numerics;
 using System.Text;
 using static System.Math;
+using static System.Numerics.Vector;
 using static System.Numerics.Vector3;
 
 Console.OutputEncoding = Encoding.UTF8;
@@ -26,7 +27,7 @@ var renderContext = new RenderContext(
   );
 
 var sw        = Stopwatch.StartNew();
-var shader    = new ApolloShader();
+var shader    = new LandscapeShader();
 var capacity  = viewPort.Width*viewPort.Height*64;
 var current   = new Buffer(capacity);
 var send      = new Buffer(capacity);
@@ -442,5 +443,84 @@ sealed class BoxShader : ShaderBase
       ? _fad*(One+Sin(_Base-new Vector3(i/33F+2*(p.X+p.Y))))
       : Zero
       );
+  }
+}
+
+sealed class LandscapeShader : ShaderBase
+{
+  readonly static Vector3 _Base = new(-0.7F,-0.2F,0.3F);
+
+  float     _time;
+  Vector2   _res;
+
+  protected override void Setup(int width, int height, double time)
+  {
+    _time=(float)time;
+    _res=new(width, height);
+  }
+
+  protected override Color Run(int x, int y)
+  {
+    float
+        d=1
+      , z=0
+      , a
+      , s
+      , t=_time
+      ;
+
+    Vector2
+        r=_res
+      , D
+      , P
+      , C
+      , S
+      ;
+
+    Vector3
+        O=Zero
+      , p
+      , R=Normalize(new(new Vector2(x,r.Y-y)-new Vector2(.5F,1)*r, r.Y))
+      , ground=new(0,2,2)
+      , sky=new(2,6,0)
+      ;
+
+    Matrix3x2
+        rot=new(1.2F,1.6F,-1.6F,1.2F,0,0)
+      ;
+    
+    for(int i=0;i<50&&d>1e-3;++i) 
+    {
+      p=z*R;
+      p.Z+=t;
+      P=.23F*(new Vector2(p.X,p.Z));
+      D=Vector2.Zero;
+      s=p.Y+4;
+      d=Abs(s)+.6F;
+      a=1;
+
+      for(int j=0;j<5;++j) 
+      {
+        (S,C)=Vector2.SinCos(P);
+        p=(new Vector3(C.X,S.X,S.X))*(new Vector3(S.Y,C.Y,S.Y));
+        D+=new Vector2(p.X,p.Y);
+        d-=a*(1+p.Z)/(1+3*Vector2.Dot(D,D));
+        P=Vector2.Transform(P,rot);
+        a*=.55F;
+      }
+
+      if(s>0) 
+      {
+        O+=One+d*ground;
+      } 
+      else 
+      {
+        O+=sky;
+      }
+
+      z+=d;
+    }
+
+    return ToColor(TanhApprox(O*1E-2F));
   }
 }
